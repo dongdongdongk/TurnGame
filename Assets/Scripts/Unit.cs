5d17ddab2a745b2af3b5d1a3fa540a32279fc8cf
dongdongdongk <dhk9309@gmail.com>
2025-11-21 09:18:31 +0900

    마우스 기능

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 targetPosition;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float stoppingDistance = 0.5f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
     }
 }
