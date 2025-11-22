8ebdfd2c6afddb5442fc43aa4a35fd75791b5294
dongdongdongk <dhk9309@gmail.com>
2025-11-23 07:14:07 +0900

    캐릭터 선택, 이동 구현

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -14,14 +20,17 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
-        }
 
-        if (Input.GetMouseButtonDown(0))
+            float rotationSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool("IsWalking", false);
         }
     }
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
