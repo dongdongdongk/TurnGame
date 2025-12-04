f8ee7ea08051425531797e7a673decfc4d90eb1c
dongdongdongk <dhk9309@gmail.com>
2025-12-04 19:18:54 +0900

    총알 발사

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
-    public event EventHandler OnShoot;
+    public event EventHandler<OnShootEventArgs> OnShoot;
+
+    public class OnShootEventArgs : EventArgs
+    {
+        public Unit targetUnit;
+        public Unit shootingUnit;
+    }
     private enum State
     {
         Aiming,
@@ -71,7 +77,11 @@ public class ShootAction : BaseAction
     }
     private void Shoot()
     {
-        OnShoot?.Invoke(this, EventArgs.Empty);
+        OnShoot?.Invoke(this, new OnShootEventArgs
+        {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
         targetUnit.Damage(40);
     }
 
