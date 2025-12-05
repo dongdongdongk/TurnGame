92937e5403917c17727b88f223cce7a886bceb22
dongdongdongk <dhk9309@gmail.com>
2025-12-05 17:09:53 +0900

    유닛 체력바, 행동 포인트 UI

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
     [SerializeField] private int health = 100;
+    private int healthMax;
+
+    private void Awake()
+    {
+        healthMax = health;
+    }
 
     public void Damage(int damageAmout)
     {
@@ -16,6 +23,8 @@ public class HealthSystem : MonoBehaviour
         {
             health = 0;
         }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -28,5 +37,10 @@ public class HealthSystem : MonoBehaviour
     private void Die()
     {
         OnDead.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
     }
 }
