99b9794b8b90367dc77c60a04328eccc8c2544dd
dongdongdongk <dhk9309@gmail.com>
2025-12-08 00:29:13 +0900

    ai 시스템

--- Assets/Scripts/Grid/LevelGrid.cs
+++ Assets/Scripts/Grid/LevelGrid.cs
@@ -9,7 +9,7 @@ public class LevelGrid : MonoBehaviour
 
     public event EventHandler OnAnyUnitMovedGridPosition;
     [SerializeField] private Transform gridDebugObjectPrefab;
-    private GridSystem gridSystem;
+    private GridSystem<GridObject> gridSystem;
     void Awake()
     {
         if (Instance != null)
@@ -20,7 +20,8 @@ public class LevelGrid : MonoBehaviour
         }
         Instance = this;
 
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem<GridObject>(10, 10, 2f, 
+            (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
 
     }
