82c597d3665257e52483f52d21fd3ec265818609
dongdongdongk <dhk9309@gmail.com>
2025-12-10 19:57:33 +0900

    스크린효과 구현

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
+    public static event EventHandler<OnShootEventArgs> OnAnyShoot;
     public event EventHandler<OnShootEventArgs> OnShoot;
 
     public class OnShootEventArgs : EventArgs
@@ -78,11 +79,19 @@ public class ShootAction : BaseAction
     }
     private void Shoot()
     {
+        OnAnyShoot?.Invoke(this, new OnShootEventArgs
+        {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
         OnShoot?.Invoke(this, new OnShootEventArgs
         {
             targetUnit = targetUnit,
             shootingUnit = unit
         });
+
+        // ScreenShake.Instance.Shake(5f);
+
         targetUnit.Damage(40);
     }
 
