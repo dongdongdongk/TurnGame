281e1f51f5dd6a7096cee5234808a57c062c22c8
dongdongdongk <dhk9309@gmail.com>
2025-12-11 19:24:59 +0900

    박스 파괴 구현

---
+++ Assets/Scripts/DestructibleCrate.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleCrate : MonoBehaviour
+{
+    public static event EventHandler OnAnyDestroyed;
+
+    [SerializeField] private Transform crateDestroyedVfxPrefab;
+
+    private GridPosition gridPosition;
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+    }
+
+    public GridPosition GetGridPosition()
+    {
+        return gridPosition;
+    }
+
+    public void Damage()
+    {
+       Transform createDestroyedTransform = Instantiate(crateDestroyedVfxPrefab, transform.position, transform.rotation);
+        
+        ApplyExplosionToChildren(createDestroyedTransform, 150f, transform.position, 10f);
+        
+        Destroy(gameObject);
+
+        OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
+    }
+}
